17e28b677bfc0b118803dc0969005b75dd43f3bb
Huzaifa246 <huzaifa24680@gmail.com>
2023-08-03 08:59:46 -0700

    not needed files

--- src/Services/generateRandomIv.js
+++
@@ -1,9 +0,0 @@
-import shuffle from "./shuffle";
-function generateRandomIv(length) {
-  let pool = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  pool = pool.repeat(5);
-  pool = shuffle(pool);
-  pool = pool.substring(0, length);
-  return pool.toString();
-}
-export default generateRandomIv;
