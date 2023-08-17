28b2e5036afd11611d4caafaeccc19aa6279829e
Huzaifa246 <huzaifa24680@gmail.com>
2023-08-17 06:44:32 -0700

    Update pass structure and ui edit

---
+++ src/Services/UpdatePassword.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { decryption, encryption } from './encryptionDecryption';
+
+async function UpdatePasswordApi(adminId, oldPassword, newPassword) {
+    try {
+        const releaseBody = {
+            adminId: adminId,
+            oldPassword: oldPassword,
+            newPassword: newPassword
+        };
+        console.log(releaseBody, "api")
+        const encryptedPostData = await encryption(releaseBody);
+        console.log(encryptedPostData, "encrypted data")
+
+
+        const response = await axios.post(`${process.env.REACT_APP_API}/api/admin/update-password`,
+            { data: encryptedPostData },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+        const encryptedData = response.data.data;
+
+        const decryptedData = await decryption(encryptedData);
+        console.log('Decrypted response:', decryptedData);
+        return decryptedData;
+    } catch (error) {
+        console.error('Error fetching data From Update Pass API:', error);
+        return [];
+    }
+}
+
+export default UpdatePasswordApi;
--- src/components/UI/Profile/Account/AdminProfile.js
+++ src/components/UI/Profile/Account/AdminProfile.js
@@ -4,12 +4,12 @@ import { useAuthData, useSidebar } from '../../../../store';
 
 const AdminProfile = () => {
     const { isSidebarOpen } = useSidebar();
-    const { authData } = useAuthData();
+    // const { authData } = useAuthData();
 
-    useEffect(() => {
-        // You can now use the authData here
-        console.log(authData);
-    }, [authData]);
+    // useEffect(() => {
+    //     // You can now use the authData here
+    //     console.log(authData);
+    // }, [authData]);
     return (
         <>
             <div className={`main-table-class ${isSidebarOpen ? 'user-table-open' : ''}`}>
@@ -19,38 +19,38 @@ const AdminProfile = () => {
                             <div className="col-xl-6 col-md-12">
                                 <div className="card user-card-full">
                                     <div className="row m-l-0 m-r-0">
-                                        <div className="col-sm-4 bg-left-grad user-profile">
+                                        <div className="col-sm-12 bg-left-grad user-profile">
                                             <div className="card-block text-center text-white">
                                                 <div className="m-b-25">
                                                     <img src="https://img.icons8.com/bubbles/100/000000/user.png" className="img-radius" alt="User-Profile-Image" />
                                                 </div>
-                                                <h6 className="f-w-600">Hembo Tingor</h6>
+                                                <h6 className="f-w-600">Admin ITS</h6>
                                                 <p>Web Designer</p>
                                                 <i className="mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
                                             </div>
                                         </div>
-                                        <div className="col-sm-8">
+                                        <div className="col-sm-12">
                                             <div className="card-block">
-                                                <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                                <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Admin Information</h6>
                                                 <div className="row">
                                                     <div className="col-sm-6">
                                                         <p className="m-b-10 f-w-600">Email</p>
-                                                        <h6 className="text-muted f-w-400">rntng@gmail.com</h6>
+                                                        <h6 className="text-muted f-w-400">TechOnVentures@gmail.com</h6>
                                                     </div>
                                                     <div className="col-sm-6">
                                                         <p className="m-b-10 f-w-600">Phone</p>
-                                                        <h6 className="text-muted f-w-400">98979989898</h6>
+                                                        <h6 className="text-muted f-w-400">+92334567890</h6>
                                                     </div>
                                                 </div>
                                                 <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
                                                 <div className="row">
                                                     <div className="col-sm-6">
-                                                        <p className="m-b-10 f-w-600">Recent</p>
-                                                        <h6 className="text-muted f-w-400">Sam Disuja</h6>
+                                                        <p className="m-b-10 f-w-600">Users</p>
+                                                        <h6 className="text-muted f-w-400">...xyz</h6>
                                                     </div>
                                                     <div className="col-sm-6">
-                                                        <p className="m-b-10 f-w-600">Most Viewed</p>
-                                                        <h6 className="text-muted f-w-400">Dinoter husainm</h6>
+                                                        <p className="m-b-10 f-w-600">More Details</p>
+                                                        <h6 className="text-muted f-w-400">XYZ</h6>
                                                     </div>
                                                 </div>
                                                 <ul className="social-link list-unstyled m-t-40 m-b-10">
